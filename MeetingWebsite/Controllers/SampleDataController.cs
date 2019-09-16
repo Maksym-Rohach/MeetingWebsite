ce8c4d822c542875414894ab4c69ae2a2b4d5a62
Maks <m.rogach777@gmail.com>
2019-09-16 18:45:52 +0300

    create branch dev

--- MeetingWebsite/Controllers/SampleDataController.cs
+++ MeetingWebsite/Controllers/SampleDataController.cs
@@ -17,6 +17,7 @@ namespace MeetingWebsite.Controllers
         [HttpGet("[action]")]
         public IEnumerable<WeatherForecast> WeatherForecasts(int startDateIndex)
         {
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
