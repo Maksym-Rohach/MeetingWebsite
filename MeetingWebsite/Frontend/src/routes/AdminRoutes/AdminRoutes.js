a95d27378097030bc9fecc41dec891261202afba
Maks <m.rogach777@gmail.com>
2019-09-25 18:12:03 +0300

    add AdminLayout

--- MeetingWebsite/Frontend/src/routes/AdminRoutes/AdminRoutes.js
+++ MeetingWebsite/Frontend/src/routes/AdminRoutes/AdminRoutes.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import UserProfile from "views/UserProfile.jsx";
+import Login from "../../components/pages/login";
 
-const User = React.lazy(() => import('../components/admin/User/User'));
-
-const routes = [
-    { path: '/admin/users', name: 'User', component: User }
+var routes = [
+  {
+    path: "/admin",
+    name: "Таблиця",
+    rtlName: "لوحة القيادة",
+    icon: "tim-icons icon-chart-pie-36",
+    component: UserProfile,
+    layout: "/admin"
+  },
+  {
+    path: "/login",
+    name: "Логін",
+    rtlName: "الرموز",
+    icon: "tim-icons icon-atom",
+    component: Login,
+    layout: "/admin"
+  }
 ];
-
 export default routes;
