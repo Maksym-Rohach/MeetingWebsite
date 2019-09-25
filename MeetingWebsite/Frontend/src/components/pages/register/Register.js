1252f9b66ba4f6d29a194faaec7688fa6e287c98
Dmytro Chepyshko <4ipik666@mail.ru>
2019-09-25 18:23:40 +0300

    update register

--- MeetingWebsite/Frontend/src/components/pages/register/Register.js
+++ MeetingWebsite/Frontend/src/components/pages/register/Register.js
@@ -43,35 +43,82 @@ class Register extends Component {
                       </InputGroup>
 
                       <InputGroup className="mb-4">
-                      <FormGroup>
-                    </FormGroup>
+                      <InputGroupAddon addonType="prepend">
+                      <i className="ml-4 fas fa-venus-mars"></i>
 
 
 
 
-                    <FormGroup>
-                      
+
+
+<p className="text-muted ml-3">Gender</p>
+
+
+
+<FormGroup check inline>
+<p className="text-muted form-check-label ml-1" check htmlFor="inline-radio1">Male</p>
+<Input className="text-muted form-check-input ml-1" type="radio" id="inline-radio1" name="inline-radios" value="option1" />
+</FormGroup>
+<FormGroup check inline>
+<p className="text-muted form-check-label ml-1" check htmlFor="inline-radio2">Female</p>
+<Input className="text-muted form-check-input ml-1" type="radio" id="inline-radio2" name="inline-radios" value="option2" />
+</FormGroup>
+
+</InputGroupAddon> 
+
+                        </InputGroup>
+{/* 
+                    <FormGroup row>
+                    <Col md="3">
+                      <Label>Inline Radios</Label>
+                    </Col>
+                    <Col md="9">
+                      <FormGroup check inline>
+                        <Input className="form-check-input" type="radio" id="inline-radio1" name="inline-radios" value="option1" />
+                        <Label className="form-check-label" check htmlFor="inline-radio1">One</Label>
+                      </FormGroup>
+                      <FormGroup check inline>
+                        <Input className="form-check-input" type="radio" id="inline-radio2" name="inline-radios" value="option2" />
+                        <Label className="form-check-label" check htmlFor="inline-radio2">Two</Label>
+                      </FormGroup>
+                
+                    </Col>
+                  </FormGroup> */}
+
+
+
+
+
+                    {/* <FormGroup>
                     <InputGroup className="mb-4">
                         <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
+                         
                             <i className="fas fa-venus-mars"></i>
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input  placeholder="Gender" autoComplete="on" />
 
-                        
-                      </InputGroup>
+                        <p className="text-muted ml-3">Gender</p>
+
+                        <FormGroup check inline>
+                        <Label className="form-check-label ml-1" check htmlFor="inline-radio1">Male</Label>
+                        <Input className="form-check-input ml-1" type="radio" id="inline-radio1" name="inline-radios" value="option1" />
+                      </FormGroup>
+                      <FormGroup check inline>
+                        <Label className="form-check-label ml-1" check htmlFor="inline-radio2">Female</Label>
+                        <Input className="form-check-input ml-1" type="radio" id="inline-radio2" name="inline-radios" value="option2" />
+                      </FormGroup>
+
+                        </InputGroupAddon> 
+                      </InputGroup> */}
 
                       {/* <p className="text-muted"><i className="fas fa-venus-mars"></i>Gender</p> */}
-                      <FormGroup check inline>
+                      {/* <FormGroup check inline>
                         <Label className="form-check-label" check htmlFor="inline-radio1">Male</Label>
                         <Input className="form-check-input" type="radio" id="inline-radio1" name="inline-radios" value="option1" />
                       </FormGroup>
                       <FormGroup check inline>
                         <Label className="form-check-label" check htmlFor="inline-radio2">Female</Label>
                         <Input className="form-check-input" type="radio" id="inline-radio2" name="inline-radios" value="option2" />
-                      </FormGroup>
-                      </FormGroup>
+                      </FormGroup> */}
+                      
 
 
                         {/* <InputGroupAddon addonType="prepend">
@@ -81,7 +128,6 @@ class Register extends Component {
                         </InputGroupAddon>
                         <Input type="password" placeholder="Gender" autoComplete="on" /> */}
 
-                      </InputGroup>
 
 
                       <InputGroup className="mb-4">
