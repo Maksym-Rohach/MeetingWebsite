161834e5960d6a5ebad27abeea89fae1793bbbfa
supernikd <supernikd@gmail.com>
2019-09-29 14:55:32 +0300

    Add Amin Controller+ViewModels

---
+++ MeetingWebsite/Areas/Admin/Controllers/NikitaControllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MeetingWebsite.DAL.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingWebsite.Areas.Admin.Controllers.NikitaControllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class AdminController : ControllerBase
+    {
+        private readonly EFDbContext _context;
+
+    }
+}
