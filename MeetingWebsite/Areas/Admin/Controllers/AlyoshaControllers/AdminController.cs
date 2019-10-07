1b67409fdd96bf0426ac36d4152e714ef7c34420
alex kunderenko <kunderenko2@gmail.com>
2019-10-07 18:44:11 +0300

    Робота с моделями

--- MeetingWebsite/Areas/Admin/Controllers/AlyoshaControllers/AdminController.cs
+++ MeetingWebsite/Areas/Admin/Controllers/AlyoshaControllers/AdminController.cs
@@ -2,12 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
-namespace MeetingWebsite.Areas.Admin.Controllers.AlyoshaController
+namespace MeetingWebsite.Areas.Admin.Controllers.AlyoshaControllers
 {
-    public class AdminController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
     {
+        // GET: api/Admin
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
 
+        // GET: api/Admin/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
 
+        // POST: api/Admin
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Admin/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
     }
 }
