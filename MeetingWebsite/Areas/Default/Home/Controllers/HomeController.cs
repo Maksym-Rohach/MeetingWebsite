22e0c81455dcdb86fe611da8963eeb47710e72fd
Mariia <rivne.st.25@gmail.com>
2019-10-13 20:24:44 +0300

    Controller and view model home

---
+++ MeetingWebsite/Areas/Default/Home/Controllers/HomeController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MeetingWebsite.Areas.Account.ViewModels;
+using MeetingWebsite.DAL.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MeetingWebsite.Helpers;
+using MeetingWebsite.Services;
+using Microsoft.EntityFrameworkCore;
+using MeetingWebsite.Areas.Default.Home.ViewModels;
+
+namespace MeetingWebsite.Areas.Default.Home.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+
+    public class HomeController : ControllerBase
+    {
+        private readonly EFDbContext _context;
+        private readonly UserManager<DbUser> _userManager;
+        private readonly SignInManager<DbUser> _signInManager;
+        private readonly IConfiguration _configuration;
+        //private readonly IFileService _fileService;       
+
+        public HomeController(EFDbContext context,
+           UserManager<DbUser> userManager,
+           SignInManager<DbUser> signInManager,
+           IConfiguration configuration
+        //IFileService fileService,      
+        )
+
+        {
+            _userManager = userManager;
+            _context = context;
+            _signInManager = signInManager;
+            _configuration = configuration;
+            //_fileService = fileService;
+        }
+        [HttpPost("home_user")]
+        public async Task<IActionResult> Users([FromBody]HomeViewModels filter)//model
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = CustomValidator.GetErrorsByModel(ModelState);
+                return BadRequest(errors);
+            }
+
+            var GenderId = _context.Gender.Where(x => x.Type == "Man").SingleOrDefault();
+
+            GetListHomeUserModel result = new GetListHomeUserModel();
+            result.CurrentPage = filter.CurrentPage;
+            var query = _context.UserProfile.AsQueryable();
+
+            if (filter.CityId > 0)
+            {
+                query = query.Where(x => x.CityId == filter.CityId);
+            }
+
+            if (filter.ZodiacId > 0)
+            {
+                query = query.Where(x => x.ZodiacId == filter.ZodiacId);
+            }
+
+            List<GetHomeUserModel> users = new List<GetHomeUserModel>();
+            result.TotalCount = query.Count();
+            users = query
+                    .Include(c => c.City)
+                    .OrderBy(u => u.Id)
+                    .Skip((filter.CurrentPage - 1) * 10)
+                    .Take(10)
+                    .Select(u => new GetHomeUserModel
+                    {
+
+                        City = u.City.Name,
+                        Status = "Status",
+                        Age = 18,
+                        Name = u.NickName,
+                        Avatar = u.Avatar
+
+                    })
+                        .ToList();
+
+            //result.GetListBoys = boys;
+
+            return Ok(result);
+        }
+
+    }
+}
---
+++ MeetingWebsite/Areas/Default/Home/ViewModels/HomeViewModels.cs
@@ -0,0 +1,37 @@
+﻿using MeetingWebsite.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetingWebsite.Areas.Default.Home.ViewModels
+{
+  
+
+        public class HomeViewModels
+        {
+            public int ZodiacId { get; set; }
+            public int Age { get; set; }
+            public string Status { get; set; }
+            public int CityId { get; set; }
+            public int CurrentPage { get; set; }
+            public int GenderId { get; set; }
+        }
+
+        public class GetHomeUserModel
+        {
+            public string Avatar { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string City { get; set; }
+            public string Status { get; set; }
+        }
+
+        public class GetListHomeUserModel
+        {
+            public List<GetHomeUserModel> GetListBoys { get; set; }
+            public int TotalCount { get; set; }
+            public int CurrentPage { get; set; }
+        }
+    }
+
