2a28ff70321a755c2cc11fefe06607bc5c15d0ae
Monstrous Gemini <fedorcuku82@gmail.com>
2019-10-14 17:02:59 +0300

    Db recreate

--- MeetingWebsite/Areas/User/Controllers/ArtemControl/UserProfileController.cs
+++ MeetingWebsite/Areas/User/Controllers/ArtemControl/UserProfileController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using MeetingWebsite.DAL.Entities;
 using System.Linq.Expressions;
 using MeetingWebsite.Areas.User.ViewModels;
+using MeetingWebsite.Areas.User.ViewModels.ArtemModels;
 
 namespace MeetingWebsite.Controllers.ArtemControl
 {
@@ -20,27 +21,28 @@ namespace MeetingWebsite.Controllers.ArtemControl
         {
             _context = context;
         }
+        
         [HttpPost("getprofile")]
-
-        public UserProfileModel GetUserProfile(string email)
+        public IActionResult GetUserProfile(GetUserProfileModel model)
         {
-            var tmp = _context.UserProfile.Where(a => a.User.Email == email).SingleOrDefault();
+            var tmp = _context.UserProfile.Where(a => a.User.Email == model.Email).SingleOrDefault();
             //if (tmp == null)
             //{
             //    return new UserProfileModel { Description = _context.UserProfile.Count().ToString() };
             //}
             var birthDate = tmp.DateOfBirth;
             int age = DateTime.Now.Year - birthDate.Year;
-            UserProfileModel user = new UserProfileModel() { 
-                NickName = tmp.NickName,
-                Age = (birthDate > DateTime.Now.AddYears(-age)) ? age-- : age,
-                City = tmp.City.Name,
-                Gender = tmp.Gender.Type,
-                Zodiac = tmp.Zodiac.Name,
-                Description = tmp.Description
-            };
-                
-            return user;
+            return Ok(
+                new UserProfileModel()
+                {
+                    NickName = tmp.NickName,
+                    Age = (birthDate > DateTime.Now.AddYears(-age)) ? age-- : age,
+                    City = tmp.City.Name,
+                    Gender = tmp.Gender.Type,
+                    Zodiac = tmp.Zodiac.Name,
+                    Description = tmp.Description
+                }
+                );
         }
         [HttpPost("setprofile")]
 
---
+++ MeetingWebsite/Areas/User/ViewModels/ArtemModels/GetUserProfileModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetingWebsite.Areas.User.ViewModels.ArtemModels
+{
+    public class GetUserProfileModel
+    {
+        public string Email { get; set; }
+    }
+}
