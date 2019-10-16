93762f99336f13373585722c124bf40fc6170279
RobotOrtem <wegasd10@gmail.com>
2019-10-16 19:51:29 +0300

    comit comit

--- MeetingWebsite/Frontend/src/components/admin/Schedule/RegistryShedule/RegistrySheduleService.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import {serverUrl} from '../../../../config';
-
-export default class RegistrySheduleService {
-    static registryShedule(model) {
-        return axios.post(`${serverUrl}api/admin/shedule-register`, model)
-       
-    };
-}
