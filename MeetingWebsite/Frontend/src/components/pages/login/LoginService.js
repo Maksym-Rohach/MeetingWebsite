9c78e1ba246ad0c20d54c9b067c0e1a4123a6887
nikitaitstep228@gmail.com <nikitaitstep228@gmail.com>
2019-10-17 19:08:27 +0300

    delete masha

--- MeetingWebsite/Frontend/src/components/pages/login/LoginService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import {serverUrl} from '../../../config';
-
-export default class LoginService {
-    static login(model) {
-        return axios.post(`${serverUrl}api/account/login`, model)
-    };
-}
--- MeetingWebsite/Frontend/src/components/refreshToken/RefreshService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import {serverUrl} from '../../config';
-export default class RefreshService {
-    static RefreshToken = () => {
-        const token = localStorage.getItem('refreshToken');
-        return axios.post(`${serverUrl}api/account/refresh/${token}`);
-    }
-}
