d0f605b340861a7e2613adf10e4a45805a7fbc67
marii4ka.dem@outlook.com <marii4ka.dem@outlook.com>
2019-10-23 21:17:52 +0300

    Update register

--- MeetingWebsite/ClientApp/src/App.js
+++ MeetingWebsite/ClientApp/src/App.js
@@ -17,7 +17,7 @@ const AdminUsers = React.lazy(() => import('./components/admin/Users'));
 const Login = React.lazy(() => import('./components/pages/login'));
 const Register = React.lazy(() => import('./components/pages/register'));
 const Home = React.lazy(() => import('./components/pages/Home'));
-const Register = React.lazy(() => import('./components/pages/register'));
+
 
 
 class App extends Component {
--- MeetingWebsite/ClientApp/src/components/pages/register/Register.js
+++ MeetingWebsite/ClientApp/src/components/pages/register/Register.js
@@ -3,6 +3,11 @@ import { Link } from 'react-router-dom';
 import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row,FormGroup,Label } from 'reactstrap';
 import DatePicker from 'react-date-picker';
 import Select from 'react-select';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import { connect } from "react-redux";
+import * as registerActions from './reducer';
+import get from "lodash.get";
 
 
 
@@ -50,6 +55,12 @@ class Register extends Component {
     password:'',
     repitPassword:'',
     city:'',
+    profileUrl: '',
+    errors: {},
+    done: false,
+    isLoading: false,
+    visible: false,
+    errorsServer: {},
 
     tmp_city: { value: '', label: 'Виберіть місто ' }, 
     date: new Date(),
@@ -57,6 +68,33 @@ class Register extends Component {
   onChange = date => this.setState({ date })
   onClick = date => this.setState({ date })
 
+  passwordVisible = (e)=>{
+    this.setState({
+      visible: !this.state.visible,
+    });
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+
+    return { isLoading: nextProps.loading, errorsServer: nextProps.errors };
+}
+
+  setStateByErrors = (name, value) => {
+    if (!!this.state.errors[name]) {
+      let errors = Object.assign({}, this.state.errors);
+      delete errors[name];
+      this.setState(
+        {
+          [name]: value,
+          errors
+        }
+      )
+    }
+    else {
+      this.setState(
+        { [name]: value })
+    }
+  }
   
 
   handleChange = (name, selectValue) => {
@@ -66,20 +104,21 @@ class Register extends Component {
   onSubmitForm = (e) => {
     e.preventDefault();
     const { nickName ,email,dateOfBirth,gender, password,repitPassword, city,errorsServer } = this.state;   
-
+    console.log("state",this.state);
     let errors = {};
     const regex_password = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{6,24}$/;
     const regex_repitPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{6,24}$/;
     const regex_email = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
 
-    if (!regex_email.test(email)) errors.email = "Не правильний формат електронної пошти!";
-    if (email === "") errors.email = "Поле не може бути пустим!";
+    if (!regex_email.test(email)&&!regex_repitPassword.test(repitPassword)) errors.email = "Не правильний формат електронної пошти!";
+    if (email === ""&&repitPassword ==="") errors.email = "Поле не може бути пустим!";
 
     if (!regex_password.test(password)&&!regex_repitPassword.test(repitPassword)) errors.password = "Пароль повинен мати мінімум 6 символів на латиниці, нижній і верхній регістр, та цифри!";
     if (password === ""&& repitPassword === "") errors.password = "Поле не може бути пустим!";
-
+    console.log("Erors",errors);
     const isValid = Object.keys(errors).length === 0
     if (isValid) {
+      console.log("true");
       this.setState({ isLoading: true });
       const model = {
         nickName:nickName,
@@ -94,6 +133,7 @@ class Register extends Component {
       this.props.Register(model);      
     }
     else {
+      console.log("false");
       this.setState({ errors });
     }
   }
@@ -139,6 +179,12 @@ class Register extends Component {
                           </InputGroupText>
                         </InputGroupAddon>
                         <Input type="text" placeholder="Електронна адреса" autoComplete="on" />
+                        className={classnames("form-control", { "is-invalid": !!errors.email })}
+                        id="email"
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.handleChange} />
+                      {!!errors.email ? <div className="invalid-feedback">{errors.email}</div> : ""}
                       </InputGroup>
 
                       <InputGroup className="mb-4">
@@ -273,6 +319,24 @@ class Register extends Component {
   }
 }
 
+Register.propTypes =
+  {
+    register: PropTypes.func.isRequired
+  }
 
+function mapStateToProps(state) {
+  return {
+    loading: get(state, 'register.post.loading'),
+    failed: get(state, 'register.post.failed'),
+    success: get(state, 'register.post.success'),
+    errors: get(state, 'register.post.errors')
+  }
+}
+
+const mapDispatch = {
+  register: (model, history) => {
+      return registerActions.register(model, history);
+  }
+}
 
 export default Register;
