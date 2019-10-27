00637c6b48f4294caede92aca3035839c5b3e836
Mariia <rivne.st.25@gmail.com>
2019-10-27 23:43:43 +0200

    Home page Photo

--- MeetingWebsite/ClientApp/src/components/pages/Home/Home.js
+++ MeetingWebsite/ClientApp/src/components/pages/Home/Home.js
@@ -6,20 +6,20 @@ import logo from './instruments/img/logo.jpg';
 const items = [
 {
   src: 'http://pngimg.com/uploads/alien/alien_PNG27.png',
-  altText: 'Slide 1',
+   altText: 'Slide 1',
   caption: 'Марсіанин',
-},
-{
-  src: 'https://i.pinimg.com/originals/d0/6a/e0/d06ae0849c47867d80afd8be7e0073a6.png',
+ },
+ {
+   src: 'https://i.pinimg.com/originals/d0/6a/e0/d06ae0849c47867d80afd8be7e0073a6.png',
   altText: 'Slide 2',
-  caption: 'Віночок',
-},
+   caption: 'Віночок',
+ },
 {
-  src: 'https://bipbap.ru/wp-content/uploads/2018/01/579a4bc07d7bf15632b7e802.png',
+   src: 'https://bipbap.ru/wp-content/uploads/2018/01/579a4bc07d7bf15632b7e802.png',
   altText: 'Slide 3',
-  caption: 'Пончик',
+   caption: 'Пончик',
 },
-];
+]; 
 
 class Home extends Component {
 
@@ -60,7 +60,7 @@ goToIndex(newIndex) {
 }
 
 render() {
-//const {activeIndex} = this.state;
+const {activeIndex} = this.state;
 
 // const slides = items.map((item)=> {
 // return (
@@ -69,21 +69,23 @@ render() {
 // </CarouselItem>
 // );
 // });
+ const slides = items.map((item) => {
+  return (
+    <CarouselItem
+      onExiting={this.onExiting}
+      onExited={this.onExited}
+      key={item.src}
+    >
 
-// const slides = items.map((item) => {
-//   return (
-//     <CarouselItem
-//       onExiting={this.onExiting}
-//       onExited={this.onExited}
-//       key={item.src}
-//     >
-//       <img className="d-block w-100" src={item.src} alt={item.altText} />
-//       <CarouselCaption 
-//       // captionText={item.caption} 
-//       captionHeader={item.caption} />
-//     </CarouselItem>
-//   );
-// });
+      <img className="d-block w-100" src={item.src} alt={item.altText} />
+
+
+      <CarouselCaption 
+      captionText={item.caption} 
+      captionHeader={item.caption} />
+   </CarouselItem>
+  );
+ });
 
       return (
         <React.Fragment>
@@ -179,6 +181,71 @@ render() {
             <div className="container">
               <div className="row align-items-center">
 
+              <Row className="container text-white  mt-5 mb-5 ">
+            <Col xs="12" sm="6" md="4">
+              <Card className="border-primary">
+                <CardBody>
+                <div><img alt ="photo" className ="img-fluid" src="https://i.pinimg.com/564x/00/97/1b/00971b1a4b0e1fd3b369cfc14b3f5a13.jpg" />
+                  </div>
+                  Місце для вашої реклами
+                  <div><a href="#" className="social-link rounded-circle text-white mr-3">See Profile</a>
+                  </div>
+                  </CardBody>
+              </Card>
+            </Col>
+            <Col xs="12" sm="6" md="4">
+              <Card className="border-primary">
+                <CardBody>
+                <div><img alt ="photo" className ="img-fluid" src="https://scontent.fdnk1-1.fna.fbcdn.net/v/t1.0-9/66881561_357366474909197_3040427990451224576_n.jpg?_nc_cat=109&_nc_oc=AQnb8qdUjE2eSwdRcT5KlqyWc1hdFs9QMNRvFQ1Wlx8Ngaw1NXM6QK7GgQAR-2ALBJE&_nc_ht=scontent.fdnk1-1.fna&oh=4c829acd2cfeeef31dd28a2be8fec660&oe=5E234BEB" />
+                  </div>
+                  Місце для вашої реклами
+                  <div><a href="#" className="social-link rounded-circle text-white mr-3">See Profile</a>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>       <Col xs="12" sm="6" md="4">
+              <Card className="border-primary">
+                <CardBody>
+                <div><img alt ="photo" className ="img-fluid" src="https://i.pinimg.com/564x/00/97/1b/00971b1a4b0e1fd3b369cfc14b3f5a13.jpg" />
+                  </div>
+                  Місце для вашої реклами
+                  <div><a href="#" className="social-link rounded-circle text-white mr-3">See Profile</a>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>       <Col xs="12" sm="6" md="4">
+              <Card className="border-primary">
+                <CardBody>
+                   <div><img alt ="photo" className ="img-fluid" src="https://i.pinimg.com/564x/9b/c7/ef/9bc7efbff02137d6048946634162b4b8.jpg" />
+                  </div>
+                  Місце для вашої реклами
+                  <div><a href="#" className="social-link rounded-circle text-white mr-3">See Profile</a>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>       <Col xs="12" sm="6" md="4">
+              <Card className="border-primary">
+                <CardBody>
+                <div><img alt ="photo" className ="img-fluid" src="https://scontent.fdnk1-1.fna.fbcdn.net/v/t1.0-9/74440160_509691536279591_223344415425429504_n.jpg?_nc_cat=108&_nc_oc=AQn06gXeZzF4m1JpXFKhf0g6RzTrwuVihYmzC98nhufF1jZFsdP84_V_f8lZcz7U0OI&_nc_ht=scontent.fdnk1-1.fna&oh=25f228cfc9ea4fe5edb97511d6ca6cd4&oe=5E52F3AB" />
+                  </div>
+                  Місце для вашої реклами
+                  <div><a href="#" className="social-link rounded-circle text-white mr-3">See Profile</a>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>       <Col xs="12" sm="6" md="4">
+              <Card className="border-primary">
+                <CardBody>
+                <div><img alt ="photo" className ="img-fluid" src="https://i.pinimg.com/originals/1f/dd/28/1fdd287d6cc483f3630ad56e283a32ae.jpg" />
+                  </div>
+                  Місце для вашої реклами
+                  <div><a href="#" className="social-link rounded-circle text-white mr-3">See Profile</a>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+{/* 
                 <div className="col-12 col-lg-6 col-xl-5 ml-sm-auto pt-lg-0 animated fadeInUp  delay-1s">
                   <h1 className="display-4 font-italic mb-2">We Offer</h1>
 
@@ -231,11 +298,15 @@ render() {
                     </a>
                   </div>
                 </div>
+               */}
+              
+              
               </div>
             </div>
           </section>
 
 
+{/* 
           <section className=" content-section bg-primary" id="chef">
             <div className="container text-white  mt-5 mb-5">
               <div className="row">
@@ -261,68 +332,51 @@ render() {
                 </div>
               </div>
             </div>
-                {/* <Row className="container text-white  mt-5 mb-5 ">
-            <Col xs="12" sm="6" md="4">
-              <Card className="border-primary">
-                <CardBody>
-                  Місце для вашої реклами
-                </CardBody>
-              </Card>
-            </Col>
-            <Col xs="12" sm="6" md="4">
-              <Card className="border-primary">
-                <CardBody>
-                  Місце для вашої реклами
-                </CardBody>
-              </Card>
-            </Col>       <Col xs="12" sm="6" md="4">
-              <Card className="border-primary">
-                <CardBody>
-                  Місце для вашої реклами
-                </CardBody>
-              </Card>
-            </Col>       <Col xs="12" sm="6" md="4">
-              <Card className="border-primary">
-                <CardBody>
-                  Місце для вашої реклами
-                </CardBody>
-              </Card>
-            </Col>       <Col xs="12" sm="6" md="4">
-              <Card className="border-primary">
-                <CardBody>
-                  Місце для вашої реклами
-                </CardBody>
-              </Card>
-            </Col>       <Col xs="12" sm="6" md="4">
-              <Card className="border-primary">
-                <CardBody>
-                  Місце для вашої реклами
-                </CardBody>
-              </Card>
-            </Col>
-          </Row> */}
-          </section>
+             
+          </section> */}
 
 
           <section className="content-section  bg-light text-black ">
             <div className="container ">
               <div className="row text-center slws-heading mb-3 animated fadeInUp  delay-1s">
                 <div className="col-md-8 offset-md-2 ">
-                  <h2 className="display-4 font-italic">Testimonials</h2>
-                  <p className="sub-heading ">Nam faucibus euismod velit, a accumsan metus. Morbi ipsum quam.</p>
+                  <h2 className="display-4 font-italic"> Що таке кохання?</h2>
+                  <p className="sub-heading ">Коли він брав у руки скрипку, все на світі зникало, залишалась лише музика.></p>
                 </div>
               </div>
-              <div className="row">
+
+                
+            <div className="animated fadeIn">
+                <Container>
+                   <Row >
+                       <Col xs="12" xl="12" >
+<Carousel  
+  className="Carousel-fixed-top blur" className="col-sm-6"
+  interval={false}
+  ride={true}
+   activeIndex={activeIndex}
+   next={this.next}
+   previous={this.previous}
+   onClickHandler={this.goToIndex}
+   ride="carousel">
+          
+     <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+     {slides}
+     <CarouselControl direction="prev" directionText="Попередній" onClickHandler={this.previous} />
+     <CarouselControl direction="next" directionText="Наступний" onClickHandler={this.next} />
+
+ </Carousel>
+
+      </Col>
+      </Row>
+
+      </Container>
+      </div> 
+
+              
+              {/* <div className="row">
                 <div className="col-sm-12 pt-5 animated fadeInUp  delay-1s">
-
                   <div id="testimonials" className="carousel slide " data-ride="carousel">
-
-                    <ol className="carousel-indicators ">
-                      <li data-target="#testimonials" data-slide-to="0" className="active"></li>
-                      <li data-target="#testimonials" data-slide-to="1"></li>
-                      <li data-target="#testimonials" data-slide-to="2"></li>
-                    </ol>
-
                     <div className="carousel-inner ">
                       <div className="item carousel-item active">
                         <div className="row">
@@ -430,7 +484,10 @@ render() {
                   </div>
                 </div>
               </div>
-            </div>
+            </div> */}
+
+</div>
+
           </section>
 
           <section className="bg-primary text-white text-center" id="services">
