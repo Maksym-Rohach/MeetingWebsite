1e562f366864a14d3885750f3370c361ef4bfbfc
Dmytro Chepyshko <4ipik666@mail.ru>
2019-11-08 15:25:43 +0200

    replace forgor_password

--- MeetingWebsite/Areas/Account/Forgot_Password/Forgot_PasswordViewModels.cs
+++ MeetingWebsite/Areas/Account/Forgot_Password/Forgot_PasswordViewModels.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
-namespace MeetingWebsite.Areas.Account.ViewModels
-{
-    public class Forgot_PasswordViewModel
-    {
+//namespace MeetingWebsite.Areas.Account.ViewModels
+//{
+//    public class Forgot_PasswordViewModel
+//    {
 
-        public string Email { get; set; }
+//        public string Email { get; set; }
 
-    }
-}
+//    }
+//}
