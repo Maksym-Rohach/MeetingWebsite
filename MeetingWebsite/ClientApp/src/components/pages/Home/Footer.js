8b4bec066b975753dda779106aca39d59ca572e2
Demianchuck <50540423+Demianchuck@users.noreply.github.com>
2019-11-10 13:16:11 +0200

    Footer

--- MeetingWebsite/ClientApp/src/components/pages/Home/Footer.js
+++ MeetingWebsite/ClientApp/src/components/pages/Home/Footer.js
@@ -3,9 +3,9 @@ class Footer extends Component {
     render() {
         return (
             <React.Fragment>
-                <footer className="footer text-center p-4">
+                <footer className="footer text-center p-4 Snavbar-fixed-bottom">
                     <div className="container">
-                        <p className="text-muted mb-0 text-light"> Made with <i className="fa fa-heart heart text-danger"></i> by Creative Tim  </p>
+                        <p className="text-muted mb-0 text-light"> Made with <i className="fa fa-heart heart text-danger"></i> by Creative Team  </p>
                     </div>
                 </footer>
             </React.Fragment>
