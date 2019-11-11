b67b8c1edc0dc8f8f4a734a47598cba7c1ff20b3
marii4ka.dem@outlook.com <marii4ka.dem@outlook.com>
2019-11-11 19:03:19 +0200

    Home page

--- MeetingWebsite/ClientApp/src/components/pages/Home/NavBar.js
+++ MeetingWebsite/ClientApp/src/components/pages/Home/NavBar.js
@@ -14,24 +14,24 @@ class NavBar extends Component {
           <Nav pills className="navbar navbar-expand-lg bg-black shadow fixed-top font-weight-bold text-uppercase">
             {/* <div className="collapse navbar-collapse "> */}
             <NavItem>
-              <Link to="/#"><img alt="bobik" className="img-fluid" src={logo} style={{ width: 40, height: 40 }} /> </Link>
+              <Link to="/#" className="mr-5"><img alt="bobik" className="img-fluid" src={logo} style={{ width: 40, height: 40 }} /> </Link>
             </NavItem>
             <NavItem>
-              <Link to="" className="social-link rounded-circle text-white mr-3">Дівчата</Link>
+              <Link to="" className="social-link rounded-circle text-white mr-5">Дівчата</Link>
             </NavItem>
             <NavItem>
-              <Link to="/boys" className="social-link rounded-circle text-white mr-3"> Хлопці</Link>
+              <Link to="/boys" className="social-link rounded-circle text-white mr-5"> Хлопці</Link>
             </NavItem>
             {/* </div>
             <div className="collapse navbar-collapse justify-content-end" id="navigation"> */}
             <NavItem>
-              <Link to="/login" className="social-link rounded-circle text-white mr-3" > Вхід</Link>
+              <Link to="/login" className="social-link rounded-circle text-white mr-5" > Вхід</Link>
             </NavItem>
             <NavItem>
-              <Link to="/register" className="social-link rounded-circle text-white mr-3"> Реєстрація</Link>
+              <Link to="/register" className="social-link rounded-circle text-white mr-5"> Реєстрація</Link>
             </NavItem>
             <NavItem>
-              <Link to="/register" className="social-link rounded-circle text-white mr-3"> Вихід</Link>
+              <Link to="/register" className="social-link rounded-circle text-white mr-5"> Вихід</Link>
             </NavItem>
             {/* </div> */}
           </Nav>
